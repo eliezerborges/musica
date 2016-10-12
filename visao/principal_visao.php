5432216a83f918585a252296d9faafdee5c6c147
Alan Jeferson <borgeseliezer1@gmail.com>
2016-10-12 13:00:37 -0300

    Adicionado CSS em menu principal

--- visao/principal_visao.php
+++ visao/principal_visao.php
@@ -2,11 +2,13 @@
 	<head>
 		<title>Menu Principal</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://unpkg.com/blaze">
 	</head>
 	<body>
-		<h2>Menu Principal</h2>
-		<a href="index.php?controlador=banda">Banda</a>
-		<a href="index.php?controlador=album">Album</a>
-		<a href="index.php?controlador=musica">Musica</a>
+		<ul class="c-nav c-nav--inline">
+		  <a class="c-nav__item" href="index.php?controlador=banda">Banda</a>
+		  <a class="c-nav__item" href="index.php?controlador=album">Album</a>
+		  <a class="c-nav__item" href="index.php?controlador=musica">Musica</a>
+		</ul>
 	</body>
 </html>
